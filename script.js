c085ba9f98bdb8edd8b094f07b941bd0a135b77b
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-01-12 20:58:00 +1100

    Generate Computer choice

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-let humanChoice = prompt("Enter your choice!\n1. Rock\n2. Scissors\n3.Paper")
-console.log(humanChoice)
+let humanChoice = prompt("Enter your choice!\n1. Rock\n2. Paper\n3. Scissors")
+
+let computerChoice = Math.floor(Math.random(5) * 3) + 1
+
+console.log(humanChoice + " " + computerChoice)

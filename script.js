ae35b8e106481f488f0d17d837b59448706597c0
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-01-15 09:06:38 +1100

    Output winner/loser message

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
-let input = prompt("Enter your choice!\nRock\nPaper\nScissors").toLowerCase()
+const choices = ["rock", "paper", "scissors"]
+// let input = ''
+// while (input in choices == false){
+//     input = prompt("Enter your choice!\nRock\nPaper\nScissors").toLowerCase()
+//     console.log(input)
+// }
 
-let choices = ["rock", "paper", "scissors"]
-let humanChoice = choices.indexOf('input') + 1
+input = prompt("Enter your choice!\nRock\nPaper\nScissors").toLowerCase()
 
+let humanChoice = choices.indexOf(input) + 1
 let computerChoice = Math.floor(Math.random(5) * 3) + 1
 
-console.log(humanChoice + " " + computerChoice)
+console.log(`You chose: ${choices[humanChoice-1]} (${humanChoice})\nComputer chose: ${choices[computerChoice-1]} (${computerChoice})`)
 
-
-
-if (humanChoice > computerChoice){
-    if (humanChoice == 3 && computerChoice == 1){
-        console.log("Computer wins!")
+function getWinner(humanChoice, computerChoice){
+    if (humanChoice > computerChoice){
+        if (humanChoice == 3 && computerChoice == 1){
+            return computerChoice
+        }
+        else {
+            return humanChoice
+        }
     }
-    else{
-        console.log("Human wins!")
+
+    else if (humanChoice < computerChoice) {
+        if (computerChoice == 3 && humanChoice == 1){
+            return humanChoice
+        } else {
+            return computerChoice
+        }
     }
+    
+    return "Tie" 
 }
 
-else {
-    if (computerChoice == 3 && humanChoice == 1){
-        console.log("Human wins!")
-    }
-    else if (computerChoice == humanChoice) {
-        console.log("Its a tie!")
-    }
-    else{
-        console.log("Computer wins!")
-    }
+
+winningChoice = getWinner(humanChoice, computerChoice)
+if (winningChoice == 'Tie') {
+    console.log(`Tie! ${choices[humanChoice - 1]} ties with ${choices[humanChoice - 1]}`)
+}
+else{
+    losingChoice = computerChoice
+    let endtext = "Win"
+    if (computerChoice == winningChoice) {losingChoice = humanChoice; endtext = "Lose"}
+    console.log(`You ${endtext}! ${choices[winningChoice - 1]} beats ${choices[losingChoice - 1]}`)
 }

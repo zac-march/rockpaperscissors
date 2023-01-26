be83a15d84be4543c792963e0441483af85084b8
Zachariah March <121649297+zac-march@users.noreply.github.com>
2023-01-27 07:49:17 +1100

    Display all game information on page

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const choices = ["rock", "paper", "scissors"]
 const choiceButtons = document.querySelectorAll('.choice')
-const resultDiv = document.querySelector('#result')
+const textDivs = document.querySelectorAll('.display')
+
+const RESULT_INDEX = 0;
+const CHOICE_INDEX = 1;
+const SCORE_INDEX = 2;
+
 let score = [0, 0]
 
 choiceButtons.forEach((button) => {
@@ -10,26 +15,22 @@ choiceButtons.forEach((button) => {
     });
 });
 
-console.log(choiceButtons)
-
-
 function updateScore(isHumanWinner){
     if (isHumanWinner){score[0] = score[0] + 1}
     else {score[1] = score[1] + 1}
-    console.log(`Human points: ${score[0]}\nComputer points: ${score[1]}`)
+    textDivs[SCORE_INDEX].textContent = `Human points: ${score[0]}\nComputer points: ${score[1]}`
 }
 
 function playRound(humanChoice) {
     computerChoice = Math.floor(Math.random() * 3)
 
-    console.log(`You chose: ${choices[humanChoice]} (${humanChoice})\nComputer chose: ${choices[computerChoice]} (${computerChoice})`)
+    textDivs[CHOICE_INDEX].textContent = `You chose: ${choices[humanChoice]} (${humanChoice})\nComputer chose: ${choices[computerChoice]} (${computerChoice})`
 
     let isHumanWinner = null;
     if (humanChoice != computerChoice){
         isHumanWinner = getIsHumanWinner(humanChoice, computerChoice)
         updateScore(isHumanWinner)
     }
-    console.log(isHumanWinner)
     displayResult(isHumanWinner, humanChoice, computerChoice)
 }
 
@@ -55,7 +56,7 @@ function getIsHumanWinner(humanChoice, computerChoice){
 function displayResult(isHumanWinner, humanChoice, computerChoice) {
 
     if (isHumanWinner == null){
-        resultDiv.textContent = `Tie! ${choices[humanChoice]} ties with ${choices[computerChoice]}`
+        textDivs[RESULT_INDEX].textContent = `Tie! ${choices[humanChoice]} ties with ${choices[computerChoice]}`
         return
     }
 
@@ -69,7 +70,7 @@ function displayResult(isHumanWinner, humanChoice, computerChoice) {
         endtext = "Lose";
     }
 
-    resultDiv.textContent = `You ${endtext}! ${choices[winningChoice]} beats ${choices[losingChoice]}`
+    textDivs[RESULT_INDEX].textContent = `You ${endtext}! ${choices[winningChoice]} beats ${choices[losingChoice]}`
     
 }
 
